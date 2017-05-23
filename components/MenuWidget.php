986bf3f74a3e9241e7cacc26e08a74616a898701
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-05-23 07:20:36 +0300

    Виджет меню категорий

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -7,26 +7,56 @@ use app\models\Category;
 
 class MenuWidget extends Widget
 {
-    public $tpl;
-    public $data;
-    public $tree;
-    public $menuHtml;
+  public $tpl;
+  public $data;
+  public $tree;
+  public $menuHtml;
 
-    public function init()
-    {
-        parent::init();
+  public function init()
+  {
+    parent::init();
 
-        if ($this->tpl === null) {
-            $this->tpl = 'menu';
-        }
-
-        $this->tpl .= '.php';
+    if ($this->tpl === null) {
+      $this->tpl = 'menu';
     }
 
-    public function run()
-    {
-        $this->data = Category::find()->asArray()->all();
-        debug($this->data);
-        return $this->tpl;
+    $this->tpl .= '.php';
+  }
+
+  public function run()
+  {
+    $this->data = Category::find()->indexBy('id')->asArray()->all();
+    $this->tree = $this->getTree();
+    $this->menuHtml = $this->getMenuHtml($this->tree);
+    //debug($this->tree);
+    return $this->menuHtml;
+  }
+
+  protected function getTree()
+  {
+    $tree = [];
+    foreach ($this->data as $id => &$node) {
+      if (!$node['parent_id'])
+        $tree[$id] = &$node;
+      else
+        $this->data[$node['parent_id']]['childs'][$node['id']] = &$node;
     }
+    return $tree;
+  }
+
+  protected function getMenuHtml($tree)
+  {
+    $str = '';
+    foreach ($tree as $category) {
+      $str .= $this->catToTemplate($category);
+    }
+    return $str;
+  }
+
+  protected function catToTemplate($category)
+  {
+    ob_start();
+    include __DIR__ . '/menu_tpl/' . $this->tpl;
+    return ob_get_clean();
+  }
 }
