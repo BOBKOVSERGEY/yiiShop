d3c4f34e5cbaf86e4513fac9de28db0cf6f4343e
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-05-25 07:54:17 +0300

    Вывод популярных товаров на главную

---
+++ controllers/AppController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bobkov
+ * Date: 25.05.2017
+ * Time: 6:56
+ */
+
+namespace app\controllers;
+
+
+use yii\base\Controller;
+
+class AppController extends Controller 
+{
+
+}
